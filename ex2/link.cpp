0a92ad075817341bb0870af315048ff14040362c
nyaachen <nyaachen@users.noreply.github.com>
2019-03-08 15:45:37 +0800

    Update link.cpp

--- ex2/link.cpp
+++ ex2/link.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cstdio>
 
 // this is actually a double end deque
 // There is no list pointer parameter in the function, which indicates
@@ -63,4 +63,109 @@ voidd insert_after(int x, int y) {
 	}
 }
 
-void delete_node(int x);
+void delete_node(int x) {
+	// find first element x
+	// use two pointer to track, just make sure relation <q,p> is always correct
+	Node *p = phead->next;
+	Node *q = phead;
+	while (p != nullptr) {
+		// p points to a node
+		if (p->x == x) {
+			// found element x, remove it from the chain, that is relation <q,p> become <q,p.next>
+			q->next = p->next;
+			// delete this node
+			delete p;
+			// quit the loop
+			break;
+		}
+		else {
+			// element x is not found. move the pointer
+			p = p->next;
+			q = q->next;
+		}
+}
+
+void print(int n) {
+	// if n is -1, print the whole list
+	if (n == -1) {
+		Node *p = phead->next;
+		while (p != nullptr) {
+			printf("%i ", p->x);
+			p = p->next;
+		}
+	}
+	else {
+		// print element no more than n
+		Node *p = phead->next;
+		for (int i=0; (i<n) && (p != nullptr); ++i) { // I prefer to use size_t as that won't overflow, however that will cause i<n act error when n is negative
+			// print the element
+			printf("%i ", p->x);
+			// move the pointer
+			p = p->next;
+		}
+	}
+	printf("\n");
+}
+
+int _reverse_print(Node *node, int n) {
+	// if this node is the tail node, check if we should print the value and return
+	if (node->next == nullptr) {
+		if ((n==-1) || n>0) { // either we need to print the whole list, or we need to print at least one element
+			printf("%i ", node->x);
+			// we printed once, return print count
+			return 1;
+		}
+		else { // we print nothing
+			return 0;
+		}
+	}
+	else { // this is not the end of the tail, so we're not sure should we print it. Instead we require a return value to judge if we have printed enough time
+		// call recursively to request print next node and get the returned print times count
+		int times;
+		times = _reverse_print(node->next, n);
+		// if print the whole list or the print count is not enough, print current value
+		if ((n==-1) || (times<n)) {
+			printf("%i ", node->x);
+			return times+1;
+		}
+		else { // we have already printed enough times.
+			return times;
+		}
+	}
+}
+
+void reverse_print(Node *node, int n) {
+	// how to use recursive to make this function?
+	// Why this function returns void? this can not make it recursively
+	// the common method is to flip the linked list and then print it
+	_reverse_print(node->next, n); // this make it to point to the first element
+	printf("\n");
+}
+
+
+void initialize_list() {
+	// This code is required as an not-initialized pointer is always trouble-maker
+	phead = new Node;
+	phead->next = nullptr;
+	ptail = phead;
+}
+
+int main() {
+	initialize_list();
+	// this code is very important
+
+	// TODO: put your code here
+	insert_at_beginning(1);
+	insert_at_beginning(2);
+	insert_at_ending(5);
+	insert_at_ending(6);
+	print(-1);
+	delete_node(6);
+	insert_after(5,55);
+	delete_node(1);
+	print(3);
+	reverse_print(-1);
+
+
+	return 0;
+}
